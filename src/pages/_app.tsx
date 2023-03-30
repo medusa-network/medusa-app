589d33883e2be209c878f5b44fe732bf8b7be077
Jonathan Easterman <jonathaneasterman@gmail.com>
2023-03-31 00:42:23 +0100

    Add plausible tracking

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,20 @@
 import 'tailwindcss/tailwind.css'
 import '@/styles/globals.css'
 import { ThemeProvider } from 'next-themes'
+import PlausibleProvider from 'next-plausible'
 
 import Web3Provider from '@/components/Web3Provider'
 import Head from 'next/head'
 
 const App = ({ Component, pageProps }) => {
   return (
-    <ThemeProvider attribute="class">
-      <Web3Provider>
-        <Component {...pageProps} />
-      </Web3Provider>
-    </ThemeProvider>
+    <PlausibleProvider domain="demo.medusanet.xyz">
+      <ThemeProvider attribute="class">
+        <Web3Provider>
+          <Component {...pageProps} />
+        </Web3Provider>
+      </ThemeProvider>
+    </PlausibleProvider>
   )
 }
 
