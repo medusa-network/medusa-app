f367aa0283bff12af8f0461b8fbb6abd287c112a
Jonathan Easterman <jonathaneasterman@gmail.com>
2023-05-02 23:47:07 +0100

    Add plausible analyitcs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,19 @@
 import 'tailwindcss/tailwind.css'
+import '@/styles/globals.css'
 import { ThemeProvider } from 'next-themes'
+import PlausibleProvider from 'next-plausible'
 
 import Web3Provider from '@/components/Web3Provider'
 
 const App = ({ Component, pageProps }) => {
   return (
-    <ThemeProvider attribute="class">
-      <Web3Provider>
-        <Component {...pageProps} />
-      </Web3Provider>
-    </ThemeProvider>
+    <PlausibleProvider domain="demo.medusanet.xyz">
+      <ThemeProvider attribute="class">
+        <Web3Provider>
+          <Component {...pageProps} />
+        </Web3Provider>
+      </ThemeProvider>
+    </PlausibleProvider>
   )
 }
 
